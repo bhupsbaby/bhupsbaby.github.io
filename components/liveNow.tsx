53ea75d15b6e52a48e22d95a18d04df7057a185d
0xpranv <pranav@greendreamearth.org>
2024-09-20 10:01:51 +0530

    replaced live lottie file with a div having animate-ping tailwind class

--- components/liveNow.tsx
+++ components/liveNow.tsx
@@ -5,13 +5,12 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const LiveNow = () => {
   return (
-    <div className="flex justify-center items-center gap-2 py-2">
-      <DotLottieReact
-        src="./animations/live-now.json"
-        className="w-6 h-6 md:w-8 md:h-8 rounded-full"
-        loop
-        autoplay
-      />
+    <div className="flex justify-center items-center gap-3 py-2">
+      <div className="flex relative items-center justify-center">
+        <div className="h-4 w-4 animate-ping rounded-full bg-green-400" />
+        <div className="absolute h-2 w-2 rounded-full bg-green-500" />
+      </div>
+
       <p className="text-xs md:text-sm font-semibold text-gray-500">
         Open to work
       </p>
